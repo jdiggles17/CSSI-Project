cab0450da446f0204aa0a4aafa96cfac991c86be
Demo User <demouser@MacBook-Air.local>
2017-07-25 11:39:02 -0700

    changes to create page

--- cssi-pathfinder-project/static_content/create.js
+++ cssi-pathfinder-project/static_content/create.js
@@ -1,3 +1,13 @@
-function submitBtn() {
-  
+function createNewEvent() {
+  $('#remove_event').on('click' , remove_last_event);
+}
+
+function remove_last_event() {
+  console.log("last event was removed")
+  $('#main').eq(-1);
+}
+
+function add_event() {
+  console.log("new event has been added")
+  $('#main').append();
 }
