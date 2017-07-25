191cb544bf2e10d9d60c9ed6d17777ee12b7f188
jdiggles17 <jdiggles@gmail.com>
2017-07-24 17:17:06 -0700

    completed the data class

--- cssi-pathfinder-project/data_classes.py
+++ cssi-pathfinder-project/data_classes.py
@@ -3,4 +3,9 @@ from google.appengine.ext import ndb
 class Event(ndb.Model):
     event_name = ndb.StringProperty()
     date = ndb.DateProperty()
-    
+    time = ndb.TimeProperty()
+    email = ndb.StringProperty()
+    address = ndb.GeoPTProperty()
+    user = ndb.StringProperty()
+    description = ndb.StringProperty()
+    tags = ndb.StringProperty(repeated = True)
